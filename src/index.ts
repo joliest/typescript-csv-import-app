7ad88d4df29c75934023547c3fac743c5ed09794
Joliver Estampador <joliverestampador@gmail.com>
2022-08-05 07:10:26 +0800

    parsing the file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
 import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    // returns a String. If we do not specify this, it will return a buffer
+    encoding: 'utf-8',
+  })
+  /**
+   * Array of rows
+   * ['28/10/2018,Man United,Everton,2,1,H,J Moss',
+   * 29/10/2018,Tottenham,Man City,0,1,A,K Friend']
+   */
+  .split('\n')
+  /**
+   * Array of of strings
+   * [...[ '27/10/2018', 'Watford', 'Huddersfield', '3', '0', 'H', 'M Dean' ] ...]
+   */
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+console.log(matches);
